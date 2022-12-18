31298a6abd193f2920a7181e00795bd2e473784d
shajeebs <shajeeb.s@gmail.com>
2022-12-18 00:27:50 +0000

    Add demo dataset
    Add demo dataset.

---
+++ datasets/sample.sql
@@ -0,0 +1,30 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'Gregg','');
+INSERT INTO "Account" VALUES(3,'Harding Julie','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2022-12-31','Non-refrigerated','Delivery 1 Item 1','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2022-12-31','Frozen','Delivery 1 Item 2','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'Delivery 1','2022-12-24T12:00:00.000+0000','Requested','2');
+COMMIT;
